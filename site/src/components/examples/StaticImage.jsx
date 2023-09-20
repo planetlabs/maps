980cabf6db2ec7e6d0f2f113b4737bd62ea1bd6e
Tim Schaub <tim@planet.com>
2023-09-19 21:49:06 -0600

    Lint tweaks
    Signed-off-by: Tim Schaub <tim@planet.com>

--- site/src/components/examples/StaticImage.jsx
+++ site/src/components/examples/StaticImage.jsx
@@ -1,26 +1,29 @@
-import Map from '../../../../lib/Map.js';
-import View from '../../../../lib/View.js';
 import Image from '../../../../lib/layer/Image.js';
 import ImageStatic from '../../../../lib/source/ImageStatic.js';
-import Projection from "ol/proj/Projection";
-import { getCenter } from 'ol/extent.js';
+import Map from '../../../../lib/Map.js';
+import Projection from 'ol/proj/Projection.js';
+import React from 'react';
+import View from '../../../../lib/View.js';
+import {getCenter} from 'ol/extent.js';
 
 const extent = [0, 0, 1024, 968];
 const projection = new Projection({
-  code: "xkcd-image",
-  units: "pixels",
+  code: 'xkcd-image',
+  units: 'pixels',
   extent,
 });
 
 function StaticImage() {
   return (
     <Map>
-      <View options={{ projection, center: getCenter(extent), zoom: 2, maxZoom: 8 }} />
+      <View
+        options={{projection, center: getCenter(extent), zoom: 2, maxZoom: 8}}
+      />
       <Image>
         <ImageStatic
           options={{
             attributions: '© <a href="http://xkcd.com/license.html">xkcd</a>',
-            url: "https://imgs.xkcd.com/comics/online_communities.png",
+            url: 'https://imgs.xkcd.com/comics/online_communities.png',
             projection,
             imageExtent: extent,
           }}
