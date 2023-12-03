51203875ace14b0fbcc3b93b163086d997df5150
Tim Schaub <tim@planet.com>
2023-12-03 12:19:12 -0800

    Using a button as a map control

---
+++ site/src/components/examples/CustomControl.jsx
@@ -0,0 +1,48 @@
+import Map from '../../../../lib/Map.js';
+import OSM from '../../../../lib/source/OSM.js';
+import React, {useCallback, useState} from 'react';
+import TileLayer from '../../../../lib/layer/WebGLTile.js';
+import View from '../../../../lib/View.js';
+
+function MapButton(props) {
+  return <button {...props}>⛶ reset view</button>;
+}
+
+const initialViewState = {center: [0, 0], zoom: 1};
+
+function CustomControl() {
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const onViewChange = useCallback(event => {
+    const view = event.target;
+    setViewState({
+      center: view.getCenter(),
+      zoom: view.getZoom(),
+    });
+  }, []);
+
+  const onButtonClick = useCallback(() => {
+    setViewState(initialViewState);
+  }, []);
+
+  return (
+    <div style={{position: 'relative', height: '100%'}}>
+      <Map>
+        <View
+          center={viewState.center}
+          zoom={viewState.zoom}
+          onChange={onViewChange}
+        />
+        <TileLayer>
+          <OSM />
+        </TileLayer>
+      </Map>
+      <MapButton
+        style={{position: 'absolute', top: '10px', right: '10px'}}
+        onClick={onButtonClick}
+      />
+    </div>
+  );
+}
+
+export default CustomControl;
