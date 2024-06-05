861346b701311cc736f5045e2fcc0fd168fee32f
Tim Schaub <tim@planet.com>
2024-06-05 09:45:22 -0600

    Test that onChange:prop works

--- tests/lib/View.test.jsx
+++ tests/lib/View.test.jsx
@@ -2,14 +2,19 @@ import Map from '../../lib/Map.js';
 import OLView from 'ol/View.js';
 import React from 'react';
 import View from '../../lib/View.js';
-import {describe, expect, it} from 'vitest';
-import {render} from '@testing-library/react';
+import {afterEach, describe, expect, it} from 'vitest';
+import {callback} from './util.js';
+import {cleanup, render} from '@testing-library/react';
+
+afterEach(() => {
+  cleanup();
+});
 
 // shared style for map target
 const style = {width: 512, height: 256};
 
 describe('<View>', () => {
-  it('adds a view to a map', async () => {
+  it('adds a view to a map', () => {
     let map;
     render(
       <div style={style}>
@@ -25,7 +30,7 @@ describe('<View>', () => {
     expect(view.getZoom()).toEqual(3);
   });
 
-  it('accepts center, zoom, and rotation props', async () => {
+  it('accepts center, zoom, and rotation props', () => {
     let map;
     render(
       <div style={style}>
@@ -40,5 +45,32 @@ describe('<View>', () => {
     expect(view.getCenter()).toEqual([1, 2]);
     expect(view.getZoom()).toEqual(3);
     expect(view.getRotation()).toEqual(4);
+  });
+
+  it('accepts a change listener', async () => {
+    const onChangeCenter = callback(event => {
+      const view = event.target;
+      return view.getCenter();
+    });
+
+    let map;
+    render(
+      <div style={style}>
+        <Map ref={r => (map = r)}>
+          <View
+            center={[1, 2]}
+            zoom={3}
+            rotation={4}
+            onChange:center={onChangeCenter.handler}
+          />
+        </Map>
+      </div>,
+    );
+
+    const view = map.getView();
+    view.setCenter([10, 20]);
+
+    const center = await onChangeCenter.called;
+    expect(center).toEqual([10, 20]);
   });
 });
